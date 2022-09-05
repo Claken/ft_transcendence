042fe3c819501107a8001da59d05b024a8dc6b22
vileleu <sinisterthebest@gmail.com>
2022-09-05 16:24:01 +0200

    first commit vileleu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,7 @@ import Navbar from './components/Navbar.tsx';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Salut</h1>
     </div>
   );
 }
