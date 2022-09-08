2665aeb883d8378aac50ecb49dc181989b02b37f
Samy CHOUAM <sachouam@e2r12p18.clusters.42paris.fr>
2022-09-08 19:31:59 +0200

    add gateway for chat + websockets and socket.io packages

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { AppGateway } from './app.gateway';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService]
+  providers: [ChatService, AppGateway, ChatGateway]
 })
 export class ChatModule {}
