d2659bff2452b9b0efa2c7a7294914df352498e0
Claken <samychouam@gmail.com>
2022-09-12 19:44:04 +0200

    add things to gateway

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,8 +1,11 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway() // decorator pour dire que la classe ChatGateway sera un gateway
 export class ChatGateway {
-  @SubscribeMessage('message')
+  @WebSocketServer()
+  server: Server;
+  @SubscribeMessage('message') // decorator pour indiquer la classe en dessous s'occupe de recevoir des messages ?
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
   }
--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
-import { AppGateway } from './app.gateway';
 import { ChatGateway } from './chat.gateway';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService, AppGateway, ChatGateway]
+  providers: [ChatService, ChatGateway]
 })
 export class ChatModule {}
