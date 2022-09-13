c09f594f0cc17c78e9182528429ecce1b0a9ec39
Samy CHOUAM <sachouam@e1r2p20.clusters.42paris.fr>
2022-09-13 18:56:43 +0200

    add hooks : OnGatewayInit, OnGatewayConnection and  OnGatewayDisconnect

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,11 +1,25 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
 @WebSocketGateway() // decorator pour dire que la classe ChatGateway sera un gateway
-export class ChatGateway {
-  @WebSocketServer()
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+
+  afterInit(server: any) {
+    
+  }
+
+  handleConnection(client: any, ...args: any[]) {
+    
+  }
+
+  handleDisconnect(client: any) {
+    
+  }
+
+  @WebSocketServer() // decorator
   server: Server;
-  @SubscribeMessage('message') // decorator pour indiquer la classe en dessous s'occupe de recevoir des messages ?
+
+  @SubscribeMessage('message') // decorator pour indiquer quelle méthode envoyer pour l'évènement dont le nom correspond à 'messages'
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
   }
