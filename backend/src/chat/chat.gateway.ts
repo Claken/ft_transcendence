780fba902f9cd8f0ff5104ffc42899c2fbdd9711
Samy CHOUAM <sachouam@e1r3p3.clusters.42paris.fr>
2022-09-17 18:18:53 +0200

    added log functions

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,26 +1,34 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect, WsResponse } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway() // decorator pour dire que la classe ChatGateway sera un gateway
-export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect { //
 
-  afterInit(server: any) {
-    
+  private logger: Logger = new Logger('ChatGateway'); //
+
+  afterInit(server: Server) {
+    this.logger.log('Initialized!');
   }
 
-  handleConnection(client: any, ...args: any[]) {
-    
+  handleConnection(client: Socket) {
+    this.logger.log(`Client ${client.id} connected`);
   }
 
-  handleDisconnect(client: any) {
-    
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client ${client.id} disconnected`);
   }
 
   @WebSocketServer() // decorator
   server: Server;
 
-  @SubscribeMessage('message') // decorator pour indiquer quelle méthode envoyer pour l'évènement dont le nom correspond à 'messages'
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @SubscribeMessage('everyone_message') // decorator pour indiquer quelle méthode envoyer pour l'évènement dont le nom correspond à 'messages'
+  SendMessageToEveryone(client: Socket, text: string): void {
+    this.server.emit('received', text);
+  }
+
+  @SubscribeMessage('MsgToServer')
+  HandleMessageToServer(client: Socket, text: string): WsResponse<string> {
+    return {event: 'MsgToClient', data: text};
   }
 }
