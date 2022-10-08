4e331f41cc4466b7c6fbdf7a977298c706a2e889
Claken <samychouam@gmail.com>
2022-10-08 23:57:02 +0200

    username

--- frontend/src/components/chat/webSocketTester.tsx
+++ frontend/src/components/chat/webSocketTester.tsx
@@ -6,10 +6,11 @@ import io, { Socket } from 'socket.io-client';
 const AppTestSockets = () => {
 
 	const	title = 'WEBSOCKETS TESTER';
-	const 	[messages, addMessages] = useState<string[]>(['Some message', 'Another message']);
+	const 	[messages, addMessages] = useState<string[]>([]);
 	const	[text, changeText] = useState<string>("");
 	const	[socket, setSocket] = useState<Socket>();
-
+	const	[username, changeUsername] = useState<string>("");
+	let 	ignore = false;
 
 	const handleChange = (event: any) => {
 		changeText(event.target.value);
@@ -31,13 +32,22 @@ const AppTestSockets = () => {
 	}
 
 	useEffect(() => {
+		if (!ignore)
+		{
+			console.log('here');
+			const newName = prompt('Enter your username: ');
+			changeUsername(newName || '');
+			ignore = true;
+		}
+	}, [])
+
+	useEffect(() => {
 		const newSocket = io('http://localhost:3001');
 		setSocket(newSocket);
 	}, [setSocket])
 
 	useEffect(() => {
 		socket?.on('msgToClient', receiveMessage);
-
 		return () => {
 			socket?.off('msgToClient', receiveMessage);
 		}
@@ -51,6 +61,8 @@ const AppTestSockets = () => {
 				<button type="submit">Send</button>
 			</form>
 			<div>
+				{/* <strong>{username}</strong> */}
+				{/* <p>{text}</p> */}
 				{messages.map((msg: string, id: number) => <p key={id}>{msg}</p>)}
 			</div>
 		</div>
