37b6ddfd46a6beb4cb60ae5e29ea84c44be5d136
mamaquig <marvyndany@gmail.com>
2022-10-12 14:19:54 +0200

    Correction d'une erreur game.module

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GameGateway } from "./game.gateway";
 import { GameController } from './game.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GameService } from './game.service';
+import { Game } from 'src/TypeOrm/Entities/game.entity';
 
 @Module({
   providers: [GameGateway, GameService],
-  controllers: [GameController]
+  controllers: [GameController],
+  imports: [TypeOrmModule.forFeature([Game])]
 })
 export class GameModule {}
