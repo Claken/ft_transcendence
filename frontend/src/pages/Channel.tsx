d452cc8e477e54768f3a7355a1551c218dc39412
vileleu <sinisterthebest@gmail.com>
2022-10-25 18:16:44 +0200

    test dm

--- frontend/src/pages/Channel.tsx
+++ frontend/src/pages/Channel.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import ProtoChat from "../components/chat/ProtoChat";
+import AppTestSockets from "../components/chat/webSocketTester";
 
 function Channel() {
-
-	return (
-		<div>
-			<h1>Channel</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Channel</h1>
+      < ProtoChat />
+      {/* < AppTestSockets /> */}
+    </div>
+  );
 }
 
 export default Channel;
