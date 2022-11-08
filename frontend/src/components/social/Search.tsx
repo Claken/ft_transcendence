94943ab5e50552cfa0919f4b0ae9addffe7273ef
earruaba <earruaba@student.42.fr>
2022-11-08 08:30:24 +0100

    social, chat color

---
+++ frontend/src/components/social/Search.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../styles/social.css";
+
+function Search() {
+  return (
+    <div className="searchContainer">
+      <div className="searchBar">
+        <input type="text" />
+      </div>
+    </div>
+  );
+}
+
+export default Search;
--- frontend/src/pages/Social.tsx
+++ frontend/src/pages/Social.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import FriendList from "../components/social/FriendList";
+import Leaderboard from "../components/social/Leaderboard";
+import Search from "../components/social/Search";
+import "../styles/social.css";
 
 function Social() {
+  const [active, setActive] = useState("Leaderboard");
 
-	return (
-		<div>
-			<h1>Social</h1>
-		</div>
-	);
+  return (
+    <div>
+      <div className="socialContainer">
+        <div className="sortButtons">
+          <button onClick={() => setActive("Leaderboard")}>Leaderboard</button>
+          <button onClick={() => setActive("FriendList")}>Friend List</button>
+          <button onClick={() => setActive("Search")}>Search</button>
+        </div>
+		{active === "Leaderboard" && <Leaderboard />}
+		{active === "FriendList" && <FriendList />}
+		{active === "Search" && <Search />}
+      </div>
+    </div>
+  );
 }
 
 export default Social;
