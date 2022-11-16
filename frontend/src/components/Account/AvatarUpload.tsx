3a046a9ac24bde8711827e5791935a70ff11680f
TonydLazuto <aderose73@gmail.com>
2022-11-16 18:59:17 +0100

    upload on change

--- frontend/src/components/Account/AvatarUpload.tsx
+++ frontend/src/components/Account/AvatarUpload.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import axios from "../../axios.config";
 
@@ -10,7 +10,21 @@ const AvatarUpload = () => {
 		setFile(e.target.files[0]);
 	};
 
-	const uploadFile = async () => {
+	const postAvatar = async (formData: FormData) => {
+		await axios
+			.post("/users/avatar", formData, {
+				withCredentials: true,
+			})
+			.then((res) => {
+				console.log(res.data);
+				// setUser(res.data)
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+	useEffect(() => {
 		if (file) {
 			const formData = new FormData();
 			/*** Generating a 10 random chars long string */
@@ -24,20 +38,10 @@ const AvatarUpload = () => {
 			// });
 			// console.log(fileConvert64);
 
-			formData.append('image', file, file.name);
-			await axios
-				.post("/users/avatar", formData, {
-					withCredentials: true,
-				})
-				.then((res) => {
-					console.log(res.data);
-					// setUser(res.data)
-				})
-				.catch((error) => {
-					console.log(error);
-				});
+			formData.append("image", file, file.name);
+			postAvatar(formData);
 		}
-	};
+	}, [file]);
 	console.log(file);
 
 	const getBase64 = async (
@@ -64,33 +68,29 @@ const AvatarUpload = () => {
 				onChange={handleFile}
 			></input>
 			<label htmlFor="file">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="icon icon-tabler icon-tabler-photo-edit"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          strokeWidth="2"
-                          stroke="currentColor"
-                          fill="none"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        >
-                          <path
-                            stroke="none"
-                            d="M0 0h24v24H0z"
-                            fill="none"
-                          ></path>
-                          <path d="M15 8h.01"></path>
-                          <path d="M11 20h-4a3 3 0 0 1 -3 -3v-10a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v4"></path>
-                          <path d="M4 15l4 -4c.928 -.893 2.072 -.893 3 0l3 3"></path>
-                          <path d="M14 14l1 -1c.31 -.298 .644 -.497 .987 -.596"></path>
-                          <path d="M18.42 15.61a2.1 2.1 0 0 1 2.97 2.97l-3.39 3.42h-3v-3l3.42 -3.39z"></path>
-                        </svg>
-                      </label>
-			<button className="btn btn-primary" onClick={uploadFile}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					className="icon icon-tabler icon-tabler-photo-edit"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					strokeWidth="2"
+					stroke="currentColor"
+					fill="none"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+				>
+					<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+					<path d="M15 8h.01"></path>
+					<path d="M11 20h-4a3 3 0 0 1 -3 -3v-10a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v4"></path>
+					<path d="M4 15l4 -4c.928 -.893 2.072 -.893 3 0l3 3"></path>
+					<path d="M14 14l1 -1c.31 -.298 .644 -.497 .987 -.596"></path>
+					<path d="M18.42 15.61a2.1 2.1 0 0 1 2.97 2.97l-3.39 3.42h-3v-3l3.42 -3.39z"></path>
+				</svg>
+			</label>
+			{/* <button className="btn btn-primary" onClick={uploadFile}>
 				Change Avatar
-			</button>
+			</button> */}
 		</div>
 	);
 };
