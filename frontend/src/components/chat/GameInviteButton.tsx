fbd0aaa6eaf0226bf345fd95df761830b8fde3aa
earruaba <earruaba@student.42.fr>
2022-12-15 20:57:35 +0100

    fix console

--- frontend/src/components/chat/GameInviteButton.tsx
+++ frontend/src/components/chat/GameInviteButton.tsx
@@ -11,7 +11,7 @@ const GameInviteButton = ({ CreateGameInvite }) => {
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            stroke-width="2"
+            strokeWidth="2"
             stroke="currentColor"
             fill="none"
             strokeLinecap="round"
